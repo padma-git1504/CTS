fc95889b8bc1adef31ca4f2a244153514edf39a3
Padma Priya <padmapriya.sk2004@gmail.com>
2025-05-27 20:30:14 +0530

    Create httpClientExample.java

---
+++ module_3/httpClientExample.java
@@ -0,0 +1,17 @@
+import java.net.URI;
+import java.net.http.*;
+import java.io.IOException;
+
+public class HttpClientExample {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com/"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status code: " + response.statusCode());
+        System.out.println("Response body:\n" + response.body());
+    }
+}
